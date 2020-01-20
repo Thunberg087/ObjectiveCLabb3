2d002be45ad71bf8c1cbb20e1b9a5928e5737de4
Jonathan Thunberg <thunberg00@gmail.com>
2020-01-20 09:02:13 +0100

    Initial Commit

---
+++ ObjectiveCLabb3/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ObjectiveCLabb3
+//
+//  Created by Jonathan Thunberg on 2020-01-20.
+//  Copyright © 2020 Jonathan Thunberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ ObjectiveCLabb3/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ObjectiveCLabb3
+//
+//  Created by Jonathan Thunberg on 2020-01-20.
+//  Copyright © 2020 Jonathan Thunberg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
